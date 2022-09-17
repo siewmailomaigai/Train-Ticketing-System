23b62eb537bfffadea1b38534a766e758e6b8889
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2022-09-17 17:10:35 +0800

    Update Reservation.java

--- Reservation/Reservation.java
+++ Reservation/Reservation.java
@@ -1,1 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Reservation;
 
+public interface Reservation {
+
+    public void reserveTicket();
+
+    public void payTicket();
+
+}
