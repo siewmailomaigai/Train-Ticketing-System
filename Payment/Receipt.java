d5233d2a9f0d38d66eb302f2262ef9f752e44b6e
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2022-09-17 16:45:59 +0800

    Update Receipt.java

--- Payment/Receipt.java
+++ Payment/Receipt.java
@@ -1,1 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payment;
 
+import Asset.Ticket;
+import Personnal.Customer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
+import java.util.Date;
+
+public class Receipt extends Transaction{
+
+   private Date datePaid;
+    private boolean paidStatus;
+    private double amountPaid;
+
+    public Receipt(Ticket ticket) {
+        super(ticket);
+    }
+    
+    public boolean isPaidStatus() {
+        return paidStatus;
+    }
+
+    public void setPaidStatus(boolean paidStatus) {
+        this.paidStatus = paidStatus;
+    }
+   
+    public void setAmountPaid(double amountPaid) {
+        this.amountPaid = amountPaid;
+    }
+   
+    public double getAmountPaid() {
+        return amountPaid;
+    }
+    
+    public void setDate(Date datePaid){
+        this.datePaid = datePaid;
+    }
+    
+    public Date getDate(){
+        return this.datePaid;
+    }
+    public static String printReceipt(Ticket ticket, Customer customer) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd");
+        LocalDateTime now = LocalDateTime.now();
+        return   "\t\t\t\t\t\t ===========================\n"
+                +"\t\t\t\t\t\t          Receipt           \n"
+                +"\t\t\t\t\t\t ===========================\n"
+                +"\t\t\t\t\t\t Customer Name  :"+customer.getName()+"\n"
+                +"\t\t\t\t\t\t Date Paid      :"+dtf.format(now)+      "\n"
+                +"\t\t\t\t\t\t Paid Status    :"+ticket.isPaidStatus()+"\n"
+                +"\t\t\t\t\t\t No.of tickets  :"+ticket.getQuantity() +"\n"
+                +"\t\t\t\t\t\t ===========================\n"
+                +"\t\t\t\t\t\t Amount paid(RM): " + ticket.calTotal()+"\n"
+                +"\t\t\t\t\t\t ===========================\n\n"
+                +"\t\t\t\t\tThank you and till next time:)";
+    }
+
+}
