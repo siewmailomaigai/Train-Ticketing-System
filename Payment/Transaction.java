90124366d64cea4b414665e119dc59104e53cef3
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2022-09-17 16:47:49 +0800

    Update Transaction.java

--- Payment/Transaction.java
+++ Payment/Transaction.java
@@ -1,1 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package payment;
 
+import Asset.Ticket;
+
+abstract public class Transaction implements Payment {
+
+    protected String paymentID;
+    protected Ticket ticket;
+    private static int nextTransactionID = 1000;
+
+    public Transaction(Ticket ticket) {
+        this.paymentID = "PMT" + Transaction.nextTransactionID;
+        this.ticket = ticket;
+        Transaction.nextTransactionID++;
+    }
+
+    public Transaction(String paymentID, Ticket ticket) {
+        this.paymentID = paymentID;
+        this.ticket = ticket;
+    }
+
+    
+}
