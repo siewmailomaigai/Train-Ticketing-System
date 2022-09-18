d868e08cb4e105149d6f311c30583109cecdd9c0
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2022-09-18 19:10:49 +0800

    Update Person.java

--- Personnel/Person.java
+++ Personnel/Person.java
@@ -1,1 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personnal;
 
+public class Person {
+
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
