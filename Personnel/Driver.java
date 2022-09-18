5dd5969e6323ff0050b6eebb7abe9fd0cb6f5879
siewmailomaigai <88498338+siewmailomaigai@users.noreply.github.com>
2022-09-18 19:10:07 +0800

    Update Driver.java

--- Personnel/Driver.java
+++ Personnel/Driver.java
@@ -1,1 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Personnal;
 
+/**
+ *
+ * @author KUNG WEI XIN
+ */
+public class Driver extends Person {
+
+    String driverID;
+    private static int nextID = 100;
+
+    public Driver(String name) {
+        super(name);
+        this.driverID = "BD" + nextID;
+        ++nextID;
+    }
+
+    public String getDriverID() {
+        return driverID;
+    }
+
+    public void setDriverID(String driverID) {
+        this.driverID = driverID;
+    }
+
+    public static int getNextID() {
+        return nextID;
+    }
+
+    public static void setNextID(int nextID) {
+        Driver.nextID = nextID;
+    }
+
+    
+    
+    public String toString() {
+        return "Driver Id: " + driverID + "\n";
+    }
+}
